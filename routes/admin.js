61464e8fc19f5c0d49353939955406ad1831586b
Mohammed Salih v <infosalihv@gmail.com>
2024-06-23 11:51:58 +0530

    routes , dependencies committed

---
+++ routes/admin.js
@@ -0,0 +1,157 @@
+const express = require('express')
+const admin_routes = express.Router()
+const Controller = require('../Controller/adminController')
+const store = require('../helpers/multer')
+const productController = require('../Controller/productController')
+const categoryController = require('../Controller/categoryController')
+const adminUserController = require('../Controller/adminUserController')
+const bannerController = require('../Controller/adminBannerController')
+const adminOrderController = require('../Controller/adminOrderManagement')
+const admincouponController = require('../Controller/admincouponController')
+const adminOfferController = require('../Controller/adminOfferController')
+const user_route = require('./user')
+
+
+
+
+const isAdmin = (req,res,next)=>{
+
+    try {
+        
+        if(req.session.admin){
+            next()
+        }else{
+            res.redirect('/admin/login')
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+// ---------ADMIN LOGIN ---------//
+
+admin_routes.get('/login' , Controller.adminLogin)
+admin_routes.get('/adminLogin', Controller.preventing)
+admin_routes.get('/adminLogout' , Controller.adminLogout)
+
+admin_routes.post('/adminValidation', Controller.adminLoginValidation)
+
+
+
+
+
+
+// ------------ADMIN  USER CONTROLLER ----------//
+
+
+
+
+admin_routes.get('/listUsers' , isAdmin , adminUserController.userManagement)
+admin_routes.get('/deleteUser/:userId', isAdmin , adminUserController.deleteUser)
+admin_routes.get('/blockUser/:userId' , isAdmin ,  adminUserController.blockUser)
+admin_routes.get('/unBlockUser/:userId', isAdmin ,  adminUserController.unBlockUser)
+
+
+
+
+
+
+// --------------ADMIN  PRODUCT CONTROLLER ----------//
+
+admin_routes.get('/productManagement', isAdmin , productController.productManagement)
+admin_routes.get('/productManagementDetail' , isAdmin , productController.productManagementDetail)
+admin_routes.get('/productManagement/addProduct', isAdmin , productController.addProduct)
+admin_routes.get('/productEdit/:id', isAdmin , productController.productEdit)
+admin_routes.get('/deleteProduct/:id',isAdmin , productController.deleteProducts)
+admin_routes.get('/deleteSoft/:id' , isAdmin , productController.softDelete)
+
+
+admin_routes.post('/productUpload', isAdmin ,  store.any() , productController.uploadProduct)
+admin_routes.post('/productUpdate/:id', isAdmin , store.any() , productController.updateProduct)
+
+
+
+
+
+// -------------- ADMIN CATEGORY CONTROLLER ------------//
+
+admin_routes.get('/categoryManagement', isAdmin , categoryController.categoryManagement)
+admin_routes.get('/addCategory', isAdmin , categoryController.addCategory)
+admin_routes.get('/editCategory/:id', isAdmin , categoryController.editCategory)
+admin_routes.get('/deleteCategory/:id', isAdmin , categoryController.deleteCategory)
+
+
+admin_routes.post('/addingNewCategory', isAdmin , categoryController.newCategory)
+admin_routes.post('/updateCategory/:id',isAdmin ,  categoryController.updateCategory)
+
+
+
+
+// ----------------- Order Management -------------------//
+
+admin_routes.get('/orderManagement' , isAdmin , adminOrderController.orderManagement)
+admin_routes.get('/moreOrderData' , isAdmin , adminOrderController.moreOrderData)
+admin_routes.post('/ordersUpdate' , isAdmin , adminOrderController.updateOrder)
+admin_routes.post('/ordersProductUpdate' , isAdmin , adminOrderController.orderProductUpdate)
+
+
+
+
+
+
+
+// ---------------- COUPAN --------------------//
+
+
+
+admin_routes.get('/couponManagement' , isAdmin , admincouponController.couponLists)
+admin_routes.get('/addCoupon' , isAdmin , admincouponController.addCouponNew)
+admin_routes.get('/editCouponPage/:couponId' , isAdmin , admincouponController.editCouponPage)
+
+admin_routes.post('/couponAdding' , isAdmin , admincouponController.couponNew)
+admin_routes.post('/CouponEdited/:couponId' , isAdmin , admincouponController.couponEdited)
+admin_routes.post('/listuser/:couponId' , isAdmin , admincouponController.listORunlist)
+admin_routes.delete('/couponDelete/:couponId' , isAdmin , admincouponController.deleteCoupon)
+
+
+
+
+// ------------ Offer ------------------------------//
+
+
+admin_routes.get('/offerManagement' , isAdmin , adminOfferController.offerPage )
+admin_routes.get('/addOffer' , isAdmin , adminOfferController.addOffer)
+
+admin_routes.post('/createOffer' , isAdmin , adminOfferController.newOffer)
+admin_routes.delete('/offerDelete/:offerId' , isAdmin , adminOfferController.deleteOffer)
+
+
+
+//---------------Dashbaord and sales report
+
+
+admin_routes.get('/adminDashboard', isAdmin , Controller.adminDashboard)
+admin_routes.get('/salesReport' , isAdmin , Controller.salesReport)
+admin_routes.get('/sales-report' , isAdmin , Controller.salesReport)
+admin_routes.get('/costum-sales-report/:reportType' , isAdmin , Controller.customSalesReport)
+admin_routes.get('/salesReport/pdf/:reportType' , isAdmin , Controller.salesReportPdf)
+admin_routes.get('/salesReport/excel/:reportType' , isAdmin , Controller.salesReportExcel)
+
+// ------------- ADMIN BANNER CONTROLLER ---------------//
+
+
+//admin_routes.get('/banner',Controller.getCouponAppled)
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = admin_routes;
---
+++ routes/user.js
@@ -0,0 +1,196 @@
+const express = require("express");
+const user_route = express.Router();
+const multer = require('multer');
+const Controller = require('../Controller/userController');
+const passport = require("../helpers/passport");
+const User = require('../models/userDB')
+const cartController = require('../Controller/cartController')
+const checkoutController = require('../Controller/checkoutController')
+const orderController = require('../Controller/orderController')
+require('dotenv').config();
+
+user_route.use(passport.initialize());
+user_route.use(passport.session());
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+
+
+user_route.get('/google/Verify', passport.authenticate('google', {
+    scope: ['email', 'profile']
+}));
+
+user_route.get('/userVerification/google', (req, res, next) => {
+    passport.authenticate('google', async (err, user, info) => {
+        if (err) {
+            return next(err);
+        }
+        if (!user) {
+            return res.redirect('/failed'); 
+        }
+        req.logIn(user, async (err) => {
+            if (err) {
+                return next(err);
+            }
+            // Pass the email to the user controller
+            await Controller.handleGoogleSuccess(req, res, user.email);
+        });
+    })(req, res, next);
+});
+
+
+
+
+const isBlocked = async (req,res,next)=>{
+
+    if(req.session.user){
+
+        userLogged = true
+        const Userdata = req.session.user
+        const userID = Userdata._id
+
+        const stsChecking = await User.findById(userID)
+
+        if(stsChecking.block){
+            req.session.destroy((err)=>{
+                if(err){
+                    console.log(err);
+                }else{
+                    res.render('user/loginPage', {locked : 'Your account has been locked'})
+                }
+            })
+            
+        }else{
+            next()
+        }
+        
+    }else{
+        userLogged = false
+        next()
+    }
+
+}
+
+
+const isLogged = (req,res,next)=>{
+
+    try {
+        
+        if(req.session.user){
+            next()
+        }else{
+            res.redirect('/login')
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+// --------- user login & validations ----------//
+
+user_route.get('/' , Controller.UserLogin)
+user_route.get('/login', Controller.loginPage)
+user_route.get('/signup' , Controller.UserSignup)
+user_route.get('/verify', Controller.verify)
+user_route.get('/resendOtp', Controller.resendOtp)
+user_route.get('/forgotPasswordPage', Controller.forgotPasswordPage)
+user_route.get('/userLogout' , isBlocked ,Controller.userLogout)
+
+
+user_route.post('/login' , Controller.loginValidation)
+user_route.post('/userRegister', Controller.userRegister)
+user_route.post('/verifyOtp' , Controller.verifyOtp)
+user_route.post('/resendOtp',Controller.resendOtp)
+user_route.post('/forgotPasswordEmail', Controller.forgotPasswordEmail)
+user_route.post('/forgotOTP' , Controller.forgotOTP)
+
+
+
+
+//------- user home & products------------//
+
+
+user_route.get('/userHome' , isLogged , isBlocked , Controller.homePagerender)
+user_route.get('/userProductlist'  ,  Controller.productList)
+user_route.get('/searchProducts?:query' , Controller.searchProduct)
+user_route.get('/productDetail?:id', Controller.productDetail)
+user_route.get('/productListAddtocart?:productId' , isLogged , isBlocked, Controller.productlistAddtocart )
+user_route.get('/addTowishlist/:productId' , isLogged , isBlocked, Controller.addToWishlist)
+user_route.get('/addTowishlistProductlist/:productId' , isLogged , isBlocked, Controller.addTowishlistProductlist)
+user_route.get('/wishlistAddtocart?:productId' , isLogged , isBlocked, Controller.wishlistAddTocart)
+
+
+user_route.post('/userProductlist', isLogged , isBlocked ,  Controller.productList)
+user_route.delete('/deleteWishlist?:productId' , isLogged , isBlocked, Controller.wishlistDelete)
+
+
+// ----------- user account --------//
+
+
+user_route.get('/userAccount', isLogged ,  isBlocked , Controller.userAccount)
+user_route.get('/ResetPassword' , isLogged , isBlocked , Controller.changePassword)
+user_route.get('/editProfileForm', isLogged , isBlocked , Controller.editProfileForm)
+user_route.get('/userAccount/addNewaddress' , isLogged , isBlocked ,  Controller.addNewaddress)
+user_route.get('/userAccount/EditAddress?:addressId', isLogged , isBlocked ,Controller.editAddress)
+user_route.get('/walletPage' , isLogged , isBlocked,  Controller.walletPage)
+user_route.get('/wishlistPage' , isLogged , isBlocked, Controller.wishlistPage)
+user_route.delete('/userAccount/DeleteAddress/:addressId' , isLogged , isBlocked , Controller.deleteAddress)
+
+
+user_route.post('/ResetPassword' , isLogged ,  isBlocked , Controller.ResetPassword)
+user_route.post('/userAccount/editedProfileData', isLogged , isBlocked ,  Controller.editedProfileData)
+user_route.post('/userAccount/newaddressAdding', isLogged , isBlocked , Controller.addingAddress)
+user_route.post('/userAccount/addressEditing?:addressId' , isLogged , isBlocked , Controller.editedNewaddress)
+user_route.post('/addToWallet' , isLogged , isBlocked, Controller.addToWallet)
+user_route.post('/razorpay-verify-wallet' , isLogged , isBlocked, Controller.walletVerifyRazorpay)
+
+
+
+// ---------- user cart ---------//
+
+user_route.get('/cart', isLogged , isBlocked ,  cartController.cart)
+user_route.get('/user/addTocart?:id' , isLogged , isBlocked ,  cartController.addToCart)
+user_route.get('/cart-Clear' , isLogged , isBlocked , cartController.cartClear)
+
+//user_route.post('/updateSelected/:productId' , isLogged , isBlocked ,cartController.updateCheckbox)
+user_route.post('/update-cart' , isLogged ,   isBlocked , cartController.changeQuantity);
+
+user_route.delete('/item-delete/:productId' , isLogged , isBlocked ,cartController.cartIteamDelete)
+
+
+
+
+//----------------------checkout --------------------//
+
+user_route.get('/checkoutPage' , isLogged , isBlocked , checkoutController.checkoutPage)
+user_route.get('/checkout/Addaddress' ,isLogged , isBlocked ,checkoutController.addAddress)
+
+
+user_route.post('/rzrpay-verify:razorpayOrderId' , isLogged , isBlocked ,  checkoutController.verifyRazorpay)
+user_route.post('/checkout/newaddressAdding', isLogged , isBlocked ,  checkoutController.checkoutaddressPost)
+user_route.post('/placeorder' , isLogged , isBlocked , upload.none(), checkoutController.placeorder)
+
+
+//--------------------Coupons------------------------//
+
+user_route.post('/applyCoupon/:enteredCouponCode' , isLogged , isBlocked, checkoutController.applyCoupon)
+user_route.post('/removeCoupon/:couponCode' , isLogged , isBlocked, checkoutController.removeCoupon)
+
+//-----------------------Orders---------------------------//
+
+
+
+user_route.get('/successOrder/:orderId' ,isLogged , isBlocked ,  orderController.successOrder)
+user_route.get('/orderView?:orderId' , isLogged , isBlocked ,orderController.ordersPage)
+// user_route.get('/productStatusReturn',isLogged , isBlocked ,  orderController.productStatusReturn);
+user_route.get('/generateInvoice/:orderId', isLogged , isBlocked , orderController.invoice)
+user_route.get('/productStatusCancel/:productId/:orderId', isLogged, isBlocked, orderController.productStatusCancel);
+
+
+user_route.post('/returnProductOrder', isLogged , isBlocked ,orderController.returnProductOrder)
+user_route.post('/returnOrder', isLogged , isBlocked ,orderController.returnOrder)
+user_route.post('/orderCancel' , isLogged , isBlocked , orderController.cancelOrder)
+
+
+module.exports = user_route
